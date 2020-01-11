a0f331e04b4ce246eb8b9189f3dc969859508865
Vytautas Liuolia <vytautas.liuolia@gmail.com>
2020-01-11 17:55:21 +0100

    Release the first beta version.

--- falcon_sqla/version.py
+++ falcon_sqla/version.py
@@ -12,5 +12,5 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-__version__ = '0.1.0a3'
+__version__ = '0.1.0b1'
 """Current version of falcon-sqla."""
